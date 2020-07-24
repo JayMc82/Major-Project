51a8ad78b4833fda42ccbed2818d5d720a868649
JayMc82 <68719686+JayMc82@users.noreply.github.com>
2020-07-23 21:28:27 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "Hotel.h"
+
+using namespace std;
+
+int main()
+{
+    /*cout << "Hotel West Side" << endl;
+    cout << "1. Add Room" << endl;
+    cout << "2. Reserve Room" << endl;
+    cout << "3. Modify Room" << endl;
+    cout << "4. View Room" << endl;
+    cout << "5. Exit" << endl;*/
+
+    HotelRoom rooms;
+
+    rooms.print();
+
+
+    return 0;
+}
